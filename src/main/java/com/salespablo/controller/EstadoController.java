f64ec3c6d05684b17a7ef07f50701a7f417b87da
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-04-17 10:15:33 -0300

    Correcoes e conclusao de insercoes de estado via hibernate

--- src/main/java/com/salespablo/controller/EstadoController.java
+++ src/main/java/com/salespablo/controller/EstadoController.java
@@ -4,7 +4,9 @@ import com.salespablo.entity.Estado;
 import com.salespablo.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,5 +20,14 @@ public class EstadoController {
         ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
         mv.addObject("estado", estado);
         return mv;
+    }
+
+    @PostMapping("/salvarEstado")
+    public ModelAndView salvar(Estado estado, BindingResult result) {
+        if(result.hasErrors()) {
+            return cadastrar(estado);
+        }
+        estadoRepository.saveAndFlush(estado);
+        return cadastrar(new Estado());
     }
 }
